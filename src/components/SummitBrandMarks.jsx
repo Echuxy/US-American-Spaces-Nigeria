64d17f2e878852ab4817b2de781a3516f3ff3e11
Echuxy <87752888+Echuxy@users.noreply.github.com>
2026-09-19 12:38:06 +0100

    Allow American Spaces logo placement beside title

--- src/components/SummitBrandMarks.jsx
+++ src/components/SummitBrandMarks.jsx
@@ -6,18 +6,20 @@ const LOGOS = {
   freedom250: 'https://freedom250.org/logo-color.png',
 }
 
-export default function SummitBrandMarks({ compact = false, light = false }) {
+export default function SummitBrandMarks({ compact = false, light = false, includeSpaces = true }) {
   return (
     <div className={`summit-brand-marks ${compact ? 'compact' : ''} ${light ? 'light' : ''}`}>
       <div className="brand-mark mission">
         <img src={LOGOS.mission} alt="United States Mission Nigeria" />
         {!compact && <span>UNITED STATES<br />MISSION NIGERIA</span>}
       </div>
-      <div className="brand-divider" />
-      <div className="brand-mark spaces">
-        <img src={LOGOS.spaces} alt="American Spaces" />
-        {!compact && <span>AMERICAN SPACES<br />NIGERIA</span>}
-      </div>
+      {includeSpaces && <>
+        <div className="brand-divider" />
+        <div className="brand-mark spaces">
+          <img src={LOGOS.spaces} alt="American Spaces" />
+          {!compact && <span>AMERICAN SPACES<br />NIGERIA</span>}
+        </div>
+      </>}
       <div className="brand-divider" />
       <div className="brand-mark freedom">
         <img src={LOGOS.freedom250} alt="Freedom 250" />
